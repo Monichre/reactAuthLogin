ca936b25e7bf37f0c85bb3b836aac5564b857023
Liam Ellis <liamhellis@gmail.com>
2017-04-01 19:05:43 -0400

    Auth0 Login successful, getting back user data

--- src/App.js
+++ src/App.js
@@ -10,12 +10,45 @@ import './App.css';
 
 
 class App extends Component {
+
+    // This sets default props. Presumably unchanging given the word 'static'
+    static defaultProps = {
+        clientId: 'YtmhCb5WOq1Gmb4FafujETZVd5NOFUAD',
+        domain: 'monichre.auth0.com'
+    }
+
+    // This is a lifecycle method instantiating our Auth0 Object (I think)
+    componentWillMount(){
+        this.lock = new Auth0Lock(this.props.clientId, this.props.domain);
+
+        // This adds a callback for an authenticated login event
+        this.lock.on('authenticated', (authResult) => {
+            console.log(authResult);
+            this.lock.getUserInfo(authResult.accessToken, (err, profile) => {
+                if(!err){
+                    console.log(profile);
+                } else {
+                    console.log(err);
+                }
+
+            });
+        });
+
+        // This binds login functions to keep this context
+        // this.login = this.login.bind(this)
+    }
+
+    //This method receives the onLoginClick from our Header Component
+    showLock(){
+        this.lock.show();
+    }
+
   render() {
     return (
-      <div className="App">
-          <Header />
-
-      </div>
+    <div className="App">
+        <Header onLoginClick={this.showLock.bind(this)}/>
+        <Home />
+    </div>
     );
   }
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,6 +4,11 @@ import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 class Header extends Component {
 
+    // We want to send this up to the main App Component - so we make it a property
+    onLoginClick(){
+        this.props.onLoginClick();
+    }
+
     render() {
         return (
 
@@ -17,7 +22,7 @@ class Header extends Component {
                 <Nav>
                     <NavItem href="#">Authors</NavItem>
                     <NavItem href="#">How It Works</NavItem>
-                    <NavItem href="#">Log In</NavItem>
+                    <NavItem onClick={this.onLoginClick.bind(this)} href="#">Log In</NavItem>
                     <NavItem href="#">Sign Up</NavItem>
                 </Nav>
             </Navbar>
